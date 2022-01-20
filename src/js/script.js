5292e2839ea774bfd998627a8aa988d3410f9c9c
YIOXIO <gekkoivan@gmail.com>
2022-01-20 13:59:34 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,67 @@
+var name = "ivan";
+let number = 7;
+const pi = 3.14;
+
+let leftBorderWidth = 200;
+
+// number
+// string
+// true/false
+// null
+// // undefined
+//     let obj ={   
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+// Symbol
+
+// alert(1234)
+// console.log(number);
+// let answer = confirm("вам есть 18 ?");
+// console.log(answer);
+
+// let answer = prompt("вам есть 18?", "");
+// console.log(answer); 
+
+// console.log(4+4)
+
+// let isChecked = true,
+//     isClose = false;
+// // console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if (2*4 == 8*1) {
+//     console.log ('Verno')
+// } else{
+//     console.log(Error)
+// }
+
+// let answer = confirm ("Vam est' 18");
+// if(answer) {
+//     console.log('Get in')
+// }   else{
+//     console.log('Get out')
+// }
+
+// const num = 50;
+// if (num <49){
+//     console.log('wrong')
+// } else if (num > 100){
+//     console.log('More')
+// } else{
+//     console.log('Verno')
+// }
+
+// for (let i = 1; i < 8; i++){
+//     console.log(i);
+// }
+
+function logging(a, b){
+    console.log(a + b)
+}
+
+logging(3, 5);
+
+logging(7, 8);
